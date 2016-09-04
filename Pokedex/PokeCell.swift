0df3752c65ec4c84b7eb7a46352f97025a3a448f
Humberto Espinola <h.espinola@icloud.com>
2016-09-04 03:02:52 -0500

    Added assets, created Pokemon Model and PokeCell view

---
+++ Pokedex/PokeCell.swift
@@ -0,0 +1,24 @@
+//
+//  PokeCell.swift
+//  Pokedex
+//
+//  Created by Isomi on 9/4/16.
+//  Copyright © 2016 Isomi. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var pokeImage: UIImageView!
+    @IBOutlet weak var pokeLabel: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        pokeImage.image = UIImage(named: "\(self.pokemon.id)")
+        pokeLabel.text = self.pokemon.name
+    }
+}
---
+++ Pokedex/Pokemon.swift
@@ -0,0 +1,27 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Isomi on 9/4/16.
+//  Copyright © 2016 Isomi. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    private var _name: String!
+    private var _id: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var id: Int {
+        return _id
+    }
+    
+    init(name: String, id: Int) {
+        self._name = name
+        self._id = id
+    }
+}
